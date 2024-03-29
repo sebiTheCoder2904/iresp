9cfbe84981520761f6fa2f675a73a40ccf5c4987
BuildTools <unconfigured@null.spigotmc.org>
2024-03-29 19:43:38 +0100

    copy ble_server to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,44 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int x;
+#include <NimBLEDevice.h>
+
+// See the following for generating UUIDs:
+// https://www.uuidgenerator.net/
+
+#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 
 void setup() {
-  // put your setup code here, to run once:
-  x = 0;
-  Serial.begin(9600);
-  Serial.println("Hello!");
+  Serial.begin(115200);
+  Serial.println("Starting BLE work!");
+
+  BLEDevice::init("Esp32c3");
+  BLEServer *pServer = BLEDevice::createServer();
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+  BLECharacteristic *pCharacteristic = pService->createCharacteristic(
+                                        CHARACTERISTIC_UUID,
+                                  /***** Enum Type NIMBLE_PROPERTY now *****      
+                                        BLECharacteristic::PROPERTY_READ   |
+                                        BLECharacteristic::PROPERTY_WRITE  
+                                        );
+                                  *****************************************/
+                                        NIMBLE_PROPERTY::READ |
+                                        NIMBLE_PROPERTY::WRITE 
+                                       );
+
+  pCharacteristic->setValue("Hello World says Neil");
+  pService->start();
+  // BLEAdvertising *pAdvertising = pServer->getAdvertising();  // this still is working for backward compatibility
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(true);
+
+
+  BLEDevice::startAdvertising();
+  Serial.println("Characteristic defined! Now you can read it in your phone!");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println(x); // Print "Hello, world!" to serial monitor
-  x++;
-  delay(100); // Wait for 1 second
+  delay(2000);
 }
