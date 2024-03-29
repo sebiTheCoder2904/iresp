626f619c3bb5e330119f852b8e24ae420ea983ad
BuildTools <unconfigured@null.spigotmc.org>
2024-03-29 19:05:24 +0100

    count!

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,18 @@
 #include <Arduino.h>
 
 // put function declarations here:
-int myFunction(int, int);
+int x;
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  x = 0;
+  Serial.begin(9600);
+  Serial.println("Hello!");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  Serial.println(x); // Print "Hello, world!" to serial monitor
+  x++;
+  delay(100); // Wait for 1 second
 }
