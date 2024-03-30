e6f56a3cfaec480b967af80a4b4aa8e930d991d4
BuildTools <unconfigured@null.spigotmc.org>
2024-03-30 18:00:26 +0100

    changed main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,30 +5,35 @@
 // See the following for generating UUIDs:
 // https://www.uuidgenerator.net/
 
-#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
-#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+#define SERVICE_UUID               "6e400001-b5a3-f393-e0a9-e50e24dcca9e"
+#define CHARACTERISTIC_UUID_read   "6e400002-b5a3-f393-e0a9-e50e24dcca9e"
+#define CHARACTERISTIC_UUID_write  "6e400003-b5a3-f393-e0a9-e50e24dcca9e"
+
+int x;
+
+BLECharacteristic *pCharacteristic_read;
+BLECharacteristic *pCharacteristic_write;
 
 void setup() {
-  Serial.begin(115200);
-  Serial.println("Starting BLE work!");
+  x = 0;
+  Serial.begin(9600);
+  Serial.println("Starting BLE work in 5 sec");
+  delay(5000);
+  Serial.println("Starting BLE work now");
 
   BLEDevice::init("Esp32c3");
   BLEServer *pServer = BLEDevice::createServer();
   BLEService *pService = pServer->createService(SERVICE_UUID);
-  BLECharacteristic *pCharacteristic = pService->createCharacteristic(
-                                        CHARACTERISTIC_UUID,
-                                  /***** Enum Type NIMBLE_PROPERTY now *****      
-                                        BLECharacteristic::PROPERTY_READ   |
-                                        BLECharacteristic::PROPERTY_WRITE  
-                                        );
-                                  *****************************************/
-                                        NIMBLE_PROPERTY::READ |
-                                        NIMBLE_PROPERTY::WRITE 
-                                       );
+  pCharacteristic_read = pService->createCharacteristic(CHARACTERISTIC_UUID_read, NIMBLE_PROPERTY::READ | NIMBLE_PROPERTY::NOTIFY);
+  pCharacteristic_write = pService->createCharacteristic(CHARACTERISTIC_UUID_write, NIMBLE_PROPERTY::WRITE);
 
-  pCharacteristic->setValue("Hello World says Neil");
+  pCharacteristic_read->setValue(4);
+
+  // Add CCCD descriptor to enable notifications
+  pCharacteristic_read->addDescriptor(new NimBLE2904());
+
+  pCharacteristic_write->setValue("test");
   pService->start();
-  // BLEAdvertising *pAdvertising = pServer->getAdvertising();  // this still is working for backward compatibility
   BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
   pAdvertising->addServiceUUID(SERVICE_UUID);
   pAdvertising->setScanResponse(true);
@@ -39,6 +44,9 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  delay(2000);
+  pCharacteristic_read->setValue(String(x) + "\n");
+  pCharacteristic_read->notify(); // Notify clients of the value change
+  //Serial.println("sent '" + String(x) + "'");
+  x++;
+  delay(25);
 }
