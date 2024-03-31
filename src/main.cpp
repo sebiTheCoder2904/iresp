4129f7c457d1af6e0505ba9ac3364ef80048474a
BuildTools <unconfigured@null.spigotmc.org>
2024-03-31 16:41:54 +0200

    add space

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 #define SERVICE_UUID               "6e400001-b5a3-f393-e0a9-e50e24dcca9e"
 #define CHARACTERISTIC_UUID_read   "6e400002-b5a3-f393-e0a9-e50e24dcca9e"
 #define CHARACTERISTIC_UUID_write  "6e400003-b5a3-f393-e0a9-e50e24dcca9e"
-
+ 
 int x;
 String old_read_value;
 
